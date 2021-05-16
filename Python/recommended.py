3b895837c7584c7bb0168ca28ccf5406f6fb4d4c
Error44 <57800447+Error4444@users.noreply.github.com>
2021-05-16 19:26:24 +0200

    Async rewrite

--- Python/recommended.py
+++ Python/recommended.py
@@ -1,9 +1,21 @@
-import requests
+# imports
+import aiohttp
+import asyncio
 
+# your api token
 kawaii_token = "token"
 
-def kawaii(main, sub, filter):
-    r = requests.get(f"https://kawaii.red/api/{main}/{sub}/token={kawaii_token}&filter={filter}/")
-    return str(r.json()["response"])
+# request
+async def kawaii(main, sub, filter):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://kawaii.red/api/{main}/{sub}/token={kawaii_token}&filter={filter}/') as r:
+            js = await r.json()
+            return str(js["response"])
 
-print(kawaii("gif", "kiss", []))
+# print async environment (normally standard in bot environment)
+async def main():
+    print(await kawaii("gif", "kiss", []))
+
+# create async environment
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
