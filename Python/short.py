b3e9814df584c3a79f0439bfade5eebe09ce52d5
Error44 <57800447+Error4444@users.noreply.github.com>
2021-05-16 19:28:56 +0200

    Async rewrite

--- Python/short.py
+++ Python/short.py
@@ -1,2 +1,14 @@
-import requests
-print(requests.get("https://kawaii.red/api/gif/kiss/token=token/").json())
+# imports
+import aiohttp
+import asyncio
+
+# request
+async def kawaii():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://kawaii.red/api/gif/endpoints/token=441192596325531648.q0LQvNdoUoUZauM3JC7Q/') as r:
+            js = await r.json()
+            print(str(js["response"]))
+
+# create async environment
+loop = asyncio.get_event_loop()
+loop.run_until_complete(kawaii())
