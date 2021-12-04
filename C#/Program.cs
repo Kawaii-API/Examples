0a050c2d811aefda4e26d37a8c333da2373a19e8
Error44 <57800447+Error4444@users.noreply.github.com>
2021-12-04 14:02:18 +0100

    Added the new c# example

---
+++ C#/Program.cs
@@ -0,0 +1,34 @@
+    //Our Handle Request gets Created.
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Json;
+    using KawaiiAPI_Request_Example;
+
+    //Make the request and make the actually call.
+    using (var client = new HttpClient())
+    {
+        try
+        {
+            //Our kawaii API token which we get on https://kawaii.red/dashboard/
+            var kawaii_API_token = "";
+            
+            //Get the Json result from the website
+            var response = await client.GetFromJsonAsync<KawaiiRedApi>(
+                string.Format("https://kawaii.red/api/gif/{0}/token={1}&filter={2}/", "kiss",
+                    kawaii_API_token, new int[] { })
+            );
+
+            //check if the response is not null
+            if (response is not null)
+            {
+                //print our final image into the Console.
+                Console.WriteLine($"Image is | {response.Response}");
+            }
+        }
+        catch (Exception e)
+        {
+            //Catches an Exception and print it into the console.
+            Console.WriteLine(e);
+            throw;
+        }
+    }
